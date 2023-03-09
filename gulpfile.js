f56fb7e5ccf0c3313762a668f27235f7cdf637ef
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-03-08 18:56:53 -0600

    after poject finished

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const { src, dest, watch, parallel} = require('gulp');
 // CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
-//const autoprefixer = require('autoprefixer');
-//const cssnano = require('cssnano');
-//const postcss = require('gulp-postcss');
-//const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 // Imagenes
 const cache = require('gulp-cache');
@@ -15,15 +15,15 @@ const webp = require('gulp-webp');
 const avif = require('gulp-avif');
 
 // Javascript
-//const terser = require('gulp-terser-js');
+const terser = require('gulp-terser-js');
 
 function css( done ) {
     src('src/scss/**/*.scss') // Identificar el archivo .SCSS a compilar
-        //.pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())
         .pipe( plumber())
         .pipe( sass() ) // Compilarlo
-        //.pipe( postcss([ autoprefixer(), cssnano() ]) )
-        //.pipe(sourcemaps.write('.'))
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe(sourcemaps.write('.'))
         .pipe( dest('build/css') ) // Almacenarla en el disco duro
     done();
 }
@@ -60,9 +60,9 @@ function versionAvif( done ) {
 
 function javascript( done ) {
     src('src/js/**/*.js')
-        //.pipe(sourcemaps.init())
-        //.pipe( terser() )
-        //.pipe(sourcemaps.write('.'))
+        .pipe(sourcemaps.init())
+        .pipe( terser() )
+        .pipe(sourcemaps.write('.'))
         .pipe(dest('build/js'));
 
     done();
@@ -74,7 +74,8 @@ function dev( done ) {
     done();
 }
 
-exports.css = css;exports.js = javascript;
+exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
